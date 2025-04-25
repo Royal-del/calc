d85bab8f2058c0b5bbe5600115e6898cd96cc786
Royal <roya.ehtesham191001@gmail.com>
2025-04-26 02:55:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-exam code
+# calculator.py
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("Addition:", add(a, b))
+print("Subtraction:", subtract(a, b))
